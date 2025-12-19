c162f63371da29ab5f0244629da435258be0a41e
Ajking420 <apurbahazra420@gmail.com>
2025-12-19 23:39:48 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,9 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-LOCAL_PATH := device/generic/SRPWK27B007
+LOCAL_PATH := device/samsung/SRPWK27B007
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+
 
 
